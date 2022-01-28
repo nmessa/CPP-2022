10b69e6263f7e0970c8607fb5af983d11aee093f
nmessa <nmessa@sau16.org>
2022-01-28 15:22:39 -0500

    Add files via upload

---
+++ Lab Exercise 2.1.2022/Problem 2/Problem 2/main.cpp
@@ -0,0 +1,26 @@
+//Lab Exercise 2/1/2022 Problem 2
+//Author: 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//Declare variable
+	string state;
+
+	//Get state abreviation from user
+	//Add code here
+
+
+	//determine and output the name of the state
+	//Add code here
+
+	
+	return 0;
+}
+
+//Output
+//Enter a state abreviation (NH, VT, ME, MA, or NY): NH
+//New Hampshire
