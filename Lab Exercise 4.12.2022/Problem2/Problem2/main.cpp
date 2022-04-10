e03b4d39f561228f259be1631c3a8d73b212578c
nmessa <nmessa@sau16.org>
2022-04-10 10:42:21 -0400

    Add files via upload

---
+++ Lab Exercise 4.12.2022/Problem2/Problem2/main.cpp
@@ -0,0 +1,15 @@
+//Lab Exercise 4/12/2022 Problem 2
+//Author: 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Add code here
+
+	return 0;
+}
+
+//Output
+//The population exceeded 200 million in 2026
