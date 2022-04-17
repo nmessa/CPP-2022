236744be0cf15a83d8b0abb1626b11e8b9684538
nmessa <nmessa@sau16.org>
2022-04-17 11:14:41 -0400

    Add files via upload

---
+++ Lab Exercise 4.20.2022/Problem 4/Problem 4/main.cpp
@@ -0,0 +1,24 @@
+//Lab Exercise 4/20/2022 Problem 4
+//Author: 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string reverse(string);
+
+int main()
+{
+	string str = "hello world";
+	cout << reverse(str) << endl;
+	return 0;
+}
+
+string reverse(string str)
+{
+	//Add code here
+
+}
+
+//Output
+//dlrow olleh
