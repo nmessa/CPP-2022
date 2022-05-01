20c2cc3666b33e3850feded8b5e8cb382f55c5aa
nmessa <nmessa@sau16.org>
2022-05-01 08:13:29 -0400

    Add files via upload

---
+++ Lab Exercise 5.2.2022/Operating System/Operating System/interuptProcess.cpp
@@ -0,0 +1,5 @@
+//Implementation of InteruptProcess class
+//Author: 
+//Date: 5.2.2022
+
+//Add code here
---
+++ Lab Exercise 5.2.2022/Operating System/Operating System/scheduler.cpp
@@ -0,0 +1,5 @@
+//Class implementation of Schedular class
+//Author: 
+//Date: 5.2.2022
+
+//Add code here
