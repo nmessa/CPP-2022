33dc678bfceb01a63fea34cf0d91d8bc4f89b572
nmessa <nmessa@sau16.org>
2022-05-08 09:48:07 -0400

    Add files via upload

---
+++ Lab Exercise 5.9.2022/Problem 1/Problem 1/main.cpp
@@ -0,0 +1,17 @@
+//Lab Exercise 5.9.20212 Problem 1
+//Author: 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Add code here
+
+
+
+	return 0;
+}
+
+//Output
+//Total = 233168
---
+++ Lab Exercise 5.9.2022/Problem 2/Problem 2/main.cpp
@@ -0,0 +1,16 @@
+//Lab Exercise 5.9.2022 Problem 2
+//Author: 
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	//Add code here
+
+	
+	return 0;
+}
+
+//Output
+//Total = 4613732
